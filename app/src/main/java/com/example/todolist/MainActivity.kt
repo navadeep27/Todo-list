829c65aef7a9de5742a9b5e473d52d8cab214e02
navadeep27 <sunnyarrolla45678@gmail.com>
2025-03-09 23:08:12 +0530

    second update

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TodoListTheme {
-        Greeting("123Android")
+        Greeting("Android")
     }
 }
