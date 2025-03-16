7825ef11af37ac876e084bb9a978d594d492ab36
navadeep27 <sunnyarrolla45678@gmail.com>
2025-03-16 14:37:24 +0530

    fourth update

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -6,22 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.example.todolist.ui.theme.TodoListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val todoViewModel = ViewModelProvider( this)[TodoViewModel::class.java]
         enableEdgeToEdge()
         setContent {
             TodoListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                     TodoListPage()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TodoListPage(todoViewModel)
                 }
+
+
             }
         }
     }
--- app/src/main/java/com/example/todolist/TodoListPage.kt
+++ app/src/main/java/com/example/todolist/TodoListPage.kt
@@ -1,12 +1,138 @@
 package com.example.todolist
 
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+
+import androidx.compose.foundation.layout.padding
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@Composable
+fun TodoListPage(viewModel: TodoViewModel){
+
+    val todoList by viewModel.todoList.observeAsState()
+    var inputText by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(40.dp)
+
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(30.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f)
+                    .clip(RoundedCornerShape(18.dp))
+                    .background(Color(0xFFF1EADC))
+                    .padding(0.dp),
+                value = inputText,
+                onValueChange = {
+                    inputText = it
+                })
+
+            Button(
+                modifier = Modifier.padding(5.dp),
+                onClick = {
+                    viewModel.addTodo(inputText)
+                    inputText = ""
+                }) {
+                Text(text = "Add")
+            }
+        }
+
+        todoList?.let {
+            LazyColumn(
+                content = {
+                    itemsIndexed(it){ index: Int, item: Todo ->
+                        TodoItem(item = item, onDelete = {
+                            viewModel.deleteTodo(item.id)
+                        })
+                    }
+                }
+            )
+        }?: Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "No items yet",
+            fontSize = 28.sp
+            )
+
+    }
+
+}
 
 
 @Composable
-fun TodoListPage(){
-    val todoList = getFakeTodo()
-    Text(text = todoList.toString())
+fun TodoItem(item : Todo,onDelete : ()-> Unit) {
+
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color(0xFFFF7F50))
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = SimpleDateFormat(  "HH:mm:aa, dd/mm" , Locale.ENGLISH).format(item.createdAt),
+                fontSize = 14.sp,
+                color = Color(0xFF00274D)
+                )
+            Text(
+                text = item.title,
+                fontSize = 25.sp,
+                color = Color(0xFFFFDAB9)
+            )
+
+        }
+        IconButton(onClick = onDelete) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_delete_outline_24),
+                contentDescription = "Delete",
+                tint = Color(0xFFFFDAB9)
+            )
+        }
+    }
 }
+
